4c5fec16b6d3ad15f174171fca589a03a63ab38a
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-10-25 19:10:58 +1100

    Delete HTML files before previewing or building

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Run this script to cleanse the Markdown files and convert them to
 # HonKit book.
 
+rm -f doc/*.html
 npm run clean
 npx honkit build
 cp -R _book/* .
--- view.sh
+++ view.sh
@@ -2,5 +2,6 @@
 
 # Run this script to build the book and view your changes locally.
 
+rm -f doc/*.html
 npm run clean
 npx honkit serve
